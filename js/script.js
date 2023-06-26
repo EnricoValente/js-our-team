5dfddc68a9e36ad2c09c243bd795ec89ae661d7e
EnricoValente <enricovalente42@gmail.com>
2023-06-26 17:23:49 +0200

    completata milestone 0

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const team = [
+    {
+        memberName: 'Warren Barnett',
+        memberRole: 'Founder & CEO',
+        memberPhoto: 'wayne-barnett-founder-ceo.jpg'
+    },
+
+    {
+        memberName: 'Angela Carroll',
+        memberRole: 'Chief Editor',
+        memberPhoto: 'angela-caroll-chief-editor.jpg' 
+    },
+
+    {
+        memberName: 'Walter Gordon',
+        memberRole: 'Office Manager',
+        memberPhoto: 'walter-gordon-office-manager.jpg' 
+    },
+
+    {
+        memberName: 'Angela Lopez',
+        memberRole: 'Social Media Manager',
+        memberPhoto: 'angela-lopez-social-media-manager.jpg' 
+    },
+
+    {
+        memberName: 'Scott Estrada',
+        memberRole: 'Developer',
+        memberPhoto: 'scott-estrada-developer.jpg' 
+    },
+
+    {
+        memberName: 'Barbara Ramos',
+        memberRole: 'Graphic Designer',
+        memberPhoto: 'barbara-ramos-graphic-designer.jpg' 
+    },
+
+    
+]
